d34c9e4c2eae7f454176b33012381823709cf841
riapatterson <riapatterson@hotmail.co.uk>
2025-08-19 17:39:19 +0100

    edited databases

--- server/database/database.sql
+++ server/database/database.sql
@@ -62,7 +62,7 @@ CREATE TABLE enemy (
     id INT GENERATED ALWAYS AS IDENTITY,
     hero_id INT NOT NULL,
     enemy_name VARCHAR(30) NOT NULL,
-    enemy_damage INT NOT NULL, 
+    enemy_XP INT NOT NULL, 
     PRIMARY KEY (id),
     FOREIGN KEY (hero_id) REFERENCES hero(id)
 )
