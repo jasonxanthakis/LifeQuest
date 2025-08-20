ff7c6fc22195d5d8d261ffe036326351a9999c65
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-08-20 14:24:28 +0100

    Added command to cut db connection in setup db

--- server/database/setup.js
+++ server/database/setup.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const fs = require('fs');
 const db = require('./connect');
+
 const sql = fs.readFileSync('./database/database.sql').toString();
 
 db.query(sql)
   .then(data => console.log("Set-up complete."))
   .catch(error => console.log(error));
+
+db.end();
