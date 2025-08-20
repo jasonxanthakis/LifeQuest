d2d62e0046a681a15233960ed4e2e3a6171aba0c
riapatterson <riapatterson@hotmail.co.uk>
2025-08-20 16:45:05 +0100

    updated tests

--- server/__tests__/server-model-unit/User.test.js
+++ server/__tests__/server-model-unit/User.test.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const {expect, it, describe, beforeAll, afterAll} = require("@jest/globals");
-const User = require("../models/User");
+const User = require('../../models/User.js');
 
 describe('user database functions', () => {
     let testUser;
     beforeAll(async () => {
         testUser = await User.create({
-            fullname: 'Eva Smith',
+            full_name: 'Eva Smith',
             username: 'EvaSmith',
             password: 'testPassword',
             email: 'email',
@@ -21,16 +21,31 @@ describe('user database functions', () => {
     });
 
     it('retrieves a user by username', async () => {
-        const foundUser = await User.getOneByUsername('Eva Smith');
+        const foundUser = await User.getOneByUsername('EvaSmith');
         expect(foundUser).toBeInstanceOf(User);
-        expect(foundUser.fullname).toBe('Eva Smith');
+        expect(foundUser.full_name).toBe('Eva Smith');
     });
 
-    it('compares password correctly', async () => {
+    it('throws error if duplicate username is created', async () => {
+            await expect (User.create({
+                full_name: 'Eva Smith',
+                username: 'EvaSmith',
+                password: 'testPassword2',
+                email: 'email2',
+                date_of_birth: '2002-08-30'
+            })).rejects.toThrow()
+        })
+
+    afterAll(async ()=>{
+        await testUser.destroy();
+    });
+    
+
+    /* it('compares password correctly', async () => {
         const match = await testUser.comparePassword('testPassword');
         expect(match).toBe(true);
 
         const wrong = await testUser.comparePassword('wrongPassword');
         expect(wrong).toBe(false);
-    });
+    });*/
 });
