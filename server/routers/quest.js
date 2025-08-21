8573a6afd63742f417aaff43c81be9da09e23820
riapatterson <riapatterson@hotmail.co.uk>
2025-08-21 15:21:27 +0100

    added quest routers

---
+++ server/routers/quest.js
@@ -0,0 +1,12 @@
+{Router} = require("express");
+QuestController = require("../controllers/quest")
+
+const router = Router();
+
+router.get("/main/:user/quests", QuestController.getQuest);
+router.post("/main/:user/quests", QuestController.createQuest);
+router.patch("/main/:user/quests/:quest/complete", QiestController.questCompleted);
+router.patch("/main/:user/quests/:quest", QuestController.modifyQuest);
+router.delete("/main/:user/quests/:quest", QuestController.destroyQuest);
+
+module.exports = router;
